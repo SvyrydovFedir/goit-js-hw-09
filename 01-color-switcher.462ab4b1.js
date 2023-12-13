f1dc3226c77f7a55d152280fa5d8272c15a6f0de
SvyrydovFedir <118449931+SvyrydovFedir@users.noreply.github.com>
2023-12-13 12:14:54 +0000

    Deploying to gh-pages from @ SvyrydovFedir/goit-js-hw-09@683dded4c98581a86662ba3c5aa7e62b1dd1f6a8 🚀

---
+++ 01-color-switcher.462ab4b1.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let d;function a(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,a(),d=setInterval(a,1e3)})),e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.462ab4b1.js.map
